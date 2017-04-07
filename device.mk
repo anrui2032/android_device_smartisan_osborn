0ebe2c478c2a1d5a0e7576d96e3a98612f6f4c4d
Glen Kuhne <kuh@google.com>
2017-04-07 14:54:17 -0700

    osborn: Set default hotspot ssid
    Added a device specific overlay that changes the default name
    from "AndroidAp" to "Smartisan U3 Pro"
    
    Test: Builds, manually verified name change
    Bug: 36368950
    Change-Id: I38a5f6fb950d6a45342080bae186a8ed276d94bf

--- device.mk
+++ device.mk
@@ -26,3 +26,7 @@ $(call inherit-product, vendor/smartisan/osborn/osborn-vendor.mk)
 
 # Inherit from sdm660-common
 $(call inherit-product, device/smartisan/sdm660-common/common.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
